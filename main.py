f4f662a9d4e23d2e3fb06875969e8f1885f1755c
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2025-01-02 05:33:50 +0000

    Added some more comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
-
+# Restarter default prefix
 bot = commands.Bot(command_prefix='r.')
-    
+# About command, with all the aliases that Restarter recognizes
 @bot.command(name='about', aliases=['information', 'info', 'support', 'botinfo', 'botinformation'])
 async def about_message(ctx):
     await ctx.send("Hello")
