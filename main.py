1d974c96d03bb0aa31c13d4da06001c20fb64cc8
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2025-01-02 20:22:13 +0000

    Add intents bc stupid

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 print("Starting Envy Rebooter")
 from discord.ext import commands
+
+# Create an instance of the bot
+intents = discord.Intents.default()
+intents.messages = True
 # Restarter default prefix
-bot = commands.Bot(command_prefix='r.')
+bot = commands.Bot(command_prefix='r.', intents=intents)
+
+# When the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
 # About command, with all the aliases that Restarter recognizes
 @bot.command(name='about', aliases=['information', 'info', 'support', 'botinfo', 'botinformation'])
 async def about_message(ctx):
